fbda0bdbc07105bef5ad094059cca8524db35116
bryan-le-dominicano <bryanlg321@gmail.com>
2024-12-02 12:42:16 -0500

    bryan's calculator
    me struggling to make a calculator

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+x=4
+y=6
+
+Addition = x+y
+Subtraction = x-y
+Multiplication = x*y
+Division = x/y 
+Power = x**y 
+
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+print(Power)
